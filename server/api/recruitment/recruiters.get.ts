ef7ecaafbc148942dca8f5965a0421ccb7f4ca9f
AJSabi <ajit.freework@gmail.com>
2026-09-02 11:59:34 +0530

    fix: restrict recruiter directory to admins

--- server/api/recruitment/recruiters.get.ts
+++ server/api/recruitment/recruiters.get.ts
@@ -1,9 +1,11 @@
 import { and, eq } from 'drizzle-orm'
 import { member, user } from '../../database/schema/auth'
+import { assertRecruitmentAdmin } from '../../utils/recruitmentVisibility'
 
 export default defineEventHandler(async (event) => {
   const session = await requirePermission(event, { application: ['read'] })
   const orgId = session.session.activeOrganizationId
+  await assertRecruitmentAdmin(orgId, session.user.id)
 
   const recruiters = await db.select({
     id: user.id,
